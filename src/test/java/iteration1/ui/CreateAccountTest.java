e2c71914a4f3d3073e76c97cc77c97eb18d387b6
alex.pshe <alexpshe.help@gmail.com>
2025-05-13 15:11:31 -0400

    Ui tests senior tests

--- src/test/java/iteration1/ui/CreateAccountTest.java
+++ src/test/java/iteration1/ui/CreateAccountTest.java
@@ -4,6 +4,8 @@ import api.models.CreateAccountResponse;
 import api.models.CreateUserRequest;
 import api.requests.steps.AdminSteps;
 import api.requests.steps.UserSteps;
+import common.annotations.UserSession;
+import common.storage.SessionStorage;
 import org.junit.jupiter.api.Test;
 import ui.pages.BankAlert;
 import ui.pages.UserDashboard;
@@ -14,15 +16,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CreateAccountTest extends BaseUiTest {
     @Test
+    @UserSession
     public void userCanCreateAccountTest() {
-        CreateUserRequest user = AdminSteps.createUser();
-
-        authAsUser(user);
-
         new UserDashboard().open().createNewAccount();
 
-        List<CreateAccountResponse> createdAccounts = new UserSteps(user.getUsername(), user.getPassword())
-                .getAllAccounts();
+        List<CreateAccountResponse> createdAccounts = SessionStorage.getSteps().getAllAccounts();
 
         assertThat(createdAccounts).hasSize(1);
 
